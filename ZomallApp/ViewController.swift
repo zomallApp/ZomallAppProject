6eba18a0b8d1ec43234b8700876f331614fbc6e4
Muhammad Usman <usmanirfan996@gmail.com>
2020-07-15 18:02:17 +0500

    Initial Commit

---
+++ ZomallApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZomallApp
+//
+//  Created by Usman on 15/07/2020.
+//  Copyright © 2020 Usman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

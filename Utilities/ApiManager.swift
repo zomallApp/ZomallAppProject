03db58ed78ff33ba8bd98a9eedd9843538971c14
Muhammad Usman <usmanirfan996@gmail.com>
2020-09-14 23:41:25 +0500

    loginApi

--- Utilities/ApiManager.swift
+++ Utilities/ApiManager.swift
@@ -34,8 +34,40 @@ class ApiManager {
 //           return sharedGlobalRecources
 //       }
     
+    
+    // check kr isko
     func loginApiCall() {
-        
+        let url = URL(string:"https://sayingz.com/api/webemaillogin")
+               
+               guard let requestUrl = url else { fatalError() }
+
+               // Prepare URL Request Object
+               var request = URLRequest(url: requestUrl)
+               request.httpMethod = "POST"
+                
+               // HTTP Request Parameters which will be sent in HTTP Request Body
+               let postString = "email=iamahmerr@gmail.com&password=12345678"
+
+               // Set HTTP Request Body
+               request.httpBody = postString.data(using: String.Encoding.utf8)
+
+               // Perform HTTP Request
+               let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                       
+                       // Check for Error
+                       if let error = error {
+                           print("Error took place \(error)")
+                           return
+                       }
+                
+                       // Convert HTTP Response Data to a String
+                       if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                           print("Response data string:\n \(dataString)")
+                       }
+               }
+               task.resume()
+
+
     }
     
 }
